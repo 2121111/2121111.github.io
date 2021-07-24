7b220fbfd3dc19cf451f4b04654e80b6e2768e5e
2121111 <87884115+2121111@users.noreply.github.com>
2021-07-24 13:03:42 +0900

    Add files via upload

--- js/sample.js
+++ js/sample.js
@@ -10,7 +10,6 @@ var data = {
 
     }]
 };
-
 var config = {
     type: 'line' ,
     data,
@@ -21,3 +20,22 @@ var lineChart = new Chart(
     document.getElementById('lineChart'),
     config,
 );
+
+const data = {
+    labels: [
+      'Eating',
+      'Drinking',
+      'Sleeping',
+      'Designing',
+      'Coding',
+      'Cycling',
+      'Running'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [65, 59, 90, 81, 56, 55, 40],
+      fill: true,
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgb(255, 99, 132)',
+      pointBackgroundColor: 'rgb(255, 99, 132)',
+    }]};
